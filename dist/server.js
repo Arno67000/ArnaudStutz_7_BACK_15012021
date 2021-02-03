e994a01d31edf46d6e09aee4b270a485035c890a
Arnaud Stutz <arno68.as@gmail.com>
2021-02-03 12:31:07 +0100

    Backend dernière version pour soutenance de projet

--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const http = require("http");
-const app_1 = require("./app");
-require('dotenv').config();
-app_1.app.set('PORT', process.env.APP_PORT);
-const server = http.createServer(app_1.app);
-server.listen(process.env.APP_PORT);
-//# sourceMappingURL=server.js.map
