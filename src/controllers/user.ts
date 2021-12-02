13bc0f3df5c197a92d629dc29276b1dec95f7c55
arno67000 <arno68.as@gmail.com>
2021-12-02 09:12:53 +0100

    improve user controller's logic

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -8,92 +8,83 @@ import { saveUser, checkUser, decodeUser, findUser, removeUser } from "../manage
 import dotenv from "dotenv";
 dotenv.config();
 
-export async function signup(req: Request, res: Response): Promise<void> {
+export async function signup(req: Request, res: Response): Promise<Response> {
     try {
         await saveUser(req.body, true);
-        res.status(201).json({ message: "New user created" });
+        return res.status(201).json({ message: "New user created" });
     } catch (error) {
         if (error instanceof ApiError) {
-            res.status(error.code).json({ Error: error.message });
-        } else {
-            res.status(500).json({ Error: error });
+            return res.status(error.code).json({ Error: error.message });
         }
+        return res.status(500).json({ Error: error });
     }
 }
 
-export async function login(req: Request, res: Response): Promise<void> {
+export async function login(req: Request, res: Response): Promise<Response> {
     try {
         const user = await checkUser(req.body.password, "pseudo", req.body.pseudo);
         const decodedUser = decodeUser(user, true);
-        res.status(200).json(decodedUser);
+        return res.status(200).json(decodedUser);
     } catch (error) {
         if (error instanceof ApiError) {
-            res.status(error.code).json({ Error: error.message });
-        } else {
-            res.status(500).json({ Error: error });
+            return res.status(error.code).json({ Error: error.message });
         }
+        return res.status(500).json({ Error: error });
     }
 }
 
-export async function deleteUser(req: Request, res: Response): Promise<void> {
+export async function deleteUser(req: Request, res: Response): Promise<Response> {
     try {
         //Comparing token id and user id
-        if (req.params.userId === req.body.allowedUser.id) {
-            const user = await findUser("id", req.params.userId);
-            if (user) {
-                await removeUser(user);
-                res.status(200).json({ message: "User removed from database" });
-            } else {
-                res.status(404).json({ error: "User not found" });
-            }
-        } else {
-            res.status(403).json({ error: "Authentication required" });
+        if (!req.body.allowedUser || req.params.userId !== req.body.allowedUser.id) {
+            return res.status(403).json({ error: "Authentication required" });
         }
+        const user = await findUser("id", req.params.userId);
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        await removeUser(user);
+        return res.status(200).json({ message: "User removed from database" });
     } catch (error) {
         if (error instanceof ApiError) {
-            res.status(error.code).json({ Error: error.message });
-        } else {
-            res.status(500).json({ Error: error });
+            return res.status(error.code).json({ Error: error.message });
         }
+        return res.status(500).json({ Error: error });
     }
 }
 
-export async function getCurrentUser(req: Request, res: Response): Promise<void> {
+export async function getCurrentUser(req: Request, res: Response): Promise<Response> {
     try {
         //Confirm token validation
         if (req.body.allowedUser) {
-            const user = await findUser("id", req.body.allowedUser.id);
-            if (user) {
-                const decodedUser = decodeUser(user);
-                res.status(200).json(decodedUser);
-            } else {
-                res.status(404).json({ message: "User not found" });
-            }
-        } else {
-            res.status(403).json({ error: "Authentication required" });
+            return res.status(403).json({ error: "Authentication required" });
         }
+        const user = await findUser("id", req.body.allowedUser.id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        const decodedUser = decodeUser(user);
+        return res.status(200).json(decodedUser);
     } catch (error) {
-        res.status(500).json({ Error: error });
+        return res.status(500).json({ Error: error });
     }
 }
 
-export async function modifyUsersPass(req: Request, res: Response): Promise<void> {
+export async function modifyUsersPass(req: Request, res: Response): Promise<Response> {
     try {
         //Comparing token id and user id
-        if (req.body.allowedUser && req.body.allowedUser.id === req.params.userId) {
-            let user = await checkUser(req.body.oldPass, "id", req.params.userId);
-            user = decodeUser(user) as User;
-            user.password = req.body.password;
-            await saveUser(user);
-            res.status(200).json({ message: "Password successfully modified" });
-        } else {
-            res.status(403).json({ message: "Authentication required" });
+        if (!req.body.allowedUser || req.params.userId !== req.body.allowedUser.id) {
+            return res.status(403).json({ error: "Authentication required" });
         }
+        let user = await checkUser(req.body.oldPass, "id", req.params.userId);
+        user = decodeUser(user) as User;
+        user.password = req.body.password;
+        await saveUser(user);
+        return res.status(200).json({ message: "Password successfully modified" });
     } catch (error) {
         if (error instanceof ApiError) {
-            res.status(error.code).json({ Error: error.message });
-        } else {
-            res.status(500).json({ Error: error });
+            return res.status(error.code).json({ Error: error.message });
         }
+        return res.status(500).json({ Error: error });
     }
 }
