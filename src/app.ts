5eb6ad5fa1cc4aec138f1b01719fac5126627431
arno67000 <arno68.as@gmail.com>
2022-01-08 11:06:44 +0100

    :adhesive_bandage: fix missing import in app

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ import swaggerUi from "swagger-ui-express";
 import helmet from "helmet";
 import morgan from "morgan";
 import { LoggerStream } from "./logger/winstonConfig";
-import { frontUrl } from "./server";
 
 //Routers
 import { userRouter } from "./routes/userRoutes";
@@ -17,7 +16,7 @@ app.use(helmet());
 app.use(morgan("combined", { stream: new LoggerStream() }));
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-    res.setHeader("Access-Control-Allow-Origin", frontUrl);
+    res.setHeader("Access-Control-Allow-Origin", process.env.FRONT_URL ?? "");
     res.setHeader(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
